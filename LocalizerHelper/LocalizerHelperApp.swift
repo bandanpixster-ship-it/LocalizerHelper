63d9fb79e086b26472822ffd86b4ad7befde0a0c
Bandan Kumar Mahto <bandan.pixster@gmail.com>
2026-06-17 11:17:04 +0530

    Initial Commit

---
+++ LocalizerHelper/LocalizerHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizerHelperApp.swift
+//  LocalizerHelper
+//
+//  Created by Bandan's MacBook Pro on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalizerHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
